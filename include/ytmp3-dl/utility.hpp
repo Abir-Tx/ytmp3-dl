950e618a86998d7d30b55f2543d55ca6be398192
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2021-07-12 21:58:01 +0600

    Add entry level configs

---
+++ include/ytmp3-dl/utility.hpp
@@ -0,0 +1,5 @@
+#if !defined(UTILITY_HPP)
+#define UTILITY_HPP
+#include <iostream>
+
+#endif // UTILITY_HPP
---
+++ ytmp3-dl.cpp
@@ -0,0 +1,9 @@
+// A Cpp code by Mushfiqur Rahman Abir AKA Abir-Tx
+// Developer Profile: https://www.github.com/Abir-Tx
+#include <iostream>
+#include "lib/jsoncpp/include/json/json.h"
+int main(int argc, char **argv)
+{
+
+	return 0;
+}
