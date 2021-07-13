07e0e74aae9256c6c6d2cefe86c97427cc03afa3
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2021-07-13 11:31:24 +0600

    Remove submodule method of jsoncpp Why: To use amalgamated version How: What: Tags: jsoncpp

--- ytmp3-dl.cpp
+++ ytmp3-dl.cpp
@@ -1,7 +1,7 @@
 // A Cpp code by Mushfiqur Rahman Abir AKA Abir-Tx
 // Developer Profile: https://www.github.com/Abir-Tx
 #include <iostream>
-#include "lib/jsoncpp/include/json/json.h"
+#include "json/json.h"
 int main(int argc, char **argv)
 {
 
