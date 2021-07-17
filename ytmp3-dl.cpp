90eb822ab943299623e468d164cc18e748e0e40d
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2021-07-17 22:49:11 +0600

    Add json config

--- ytmp3-dl.cpp
+++ ytmp3-dl.cpp
@@ -3,8 +3,21 @@
 #include "include/alib/alib.hpp"
 #include <iostream>
 #include "include/json/json.h"
+
+#include <fstream>
 int main(int argc, char **argv)
 {
-	alib::decorateMe("YTMP3-DL", 1, " ", true);
+	/* Json config condes & settings */
+	std::string config_file = "config.json";
+
+	std::ifstream config(config_file);
+	Json::Value allData;
+	config >> allData;
+
+	if (allData.get("enableDecor", false).asBool())
+		alib::decorateMe("YTMP3-DL", 1, " ", true);
+	if (allData["enableColoredLines"])
+		alib::horizontalLine(1, allData.get("lineColor", "white").asString());
+
 	return 0;
 }
